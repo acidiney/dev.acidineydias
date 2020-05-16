07832f2ceb9239e2ed23d1fda648f35d6befe2b2
Carlos Garcia <gtxcarl9@gmail.com>
2020-05-16 23:00:39 +0100

    Added github project  link

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="root flex flex-col md:flex-row md:fixed md:overflow-hidden">
+          <a id="github-link" href="https://github.com/codepro-ao/Grid-Layout-Flexbox" target="_blank" rel="noopener noreferrer"  class="githubCorne" aria-label="View source on Github"><svg viewBox="0 0 250 250" aria-hidden="true" class="githubCornerSVG"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" className="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
     <div class="about-me pt-10 md:py-10 md:h-full md:w-6/12 flex flex-col">
       <header class="flex flex-col">
         <figure>
@@ -17,14 +18,14 @@
         </h1>
 
         <p>
-          I'm a full stack developer, currently working as a Frontend Lead at <a
+          I'm full stack developer, currently working as Frontend Lead at
+          <a
             href="https://www.digitalfactory.co.ao"
             target="__blank"
-          >Digital Factory Angola</a>. 
-          I'm a frontend addict and deeply enjoy creating wonderful user experiences, 
-          I have a strong passion for CSS and I can't keep my mouth closed when I discover a new possibilities. 
-          I love to see my systems working from front to back. I share my learnings on Youtube/Medium 
-          and anywhere else I can be heard.
+          >Digital Factory Angola</a>. I'm a frontend addict and user experience, I love css with passion
+          and always keep my mouth open when I discover a new possibilities. I
+          love to see my systems working from front to back. I share my
+          learnings on Youtube/Medium and anywhere that accepts me.
         </p>
         <a class="text-left md:mr-5 block" href="https://drive.google.com/open?id=1YMoSAyeULgir2hk9fp7MAvzqGl-RUm74" target="__blank">
           <i class="icon-download"></i> Download my CV.
@@ -57,7 +58,7 @@
             :key="article.link"
             v-observe-visibility="(isVisible, entry) => loadFrontMatterDescription(isVisible, entry, article.link)"
             class="py-6 px-6 relative">
-            <nuxt-link :to="`/blog/${article.link}`">
+            <nuxt-link :to="`/post/${article.link}`">
               <header>
                 <div class="categories flex-wrap flex">
                   <p v-for="(category, i) in article.categories" :key="category + i" class="mr-2 text-xs rounded">
@@ -275,7 +276,24 @@ export default {
   font-weight: 100;
 }
 
+/* open-source */
+ .githubCornerSVG{
+    fill:#1db954;
+    color:#fff;
+    position: absolute;
+    top: 0!important;
+    border: 0;
+    right: 0;
+    z-index: 999;
+    height: 65px;
+    width:65px;
+}
+
 @media (min-width: 768px) {
+  .githubCornerSVG{
+        height: 50px;
+        width: 50px;
+  }
   .root {
     height: 100vh;
   }
