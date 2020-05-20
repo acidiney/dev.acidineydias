959a3029115a77a112146110ad96ab41d04b18e2
Acidiney Dias <acidiney.dias@digitalfactory.co.ao>
2020-05-20 10:47:33 +0100

    fix: analitcs

--- plugins/ga.js
+++ plugins/ga.js
@@ -8,7 +8,7 @@ export default ({ app }) => {
     }, i[r].l = 1 * new Date(); a = s.createElement(o),
       m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
     })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    ga('create', '111254529-3', 'auto')
+    ga('create', 'UA-111254529-3', 'auto')
     app.router.afterEach((to, from) => {
       ga('set', 'page', to.fullPath)
       ga('send', 'pageview')
