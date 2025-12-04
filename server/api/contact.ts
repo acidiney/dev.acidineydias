19b077b51e7a55c4daa32d5ae5725776a7f6501a
Acidiney Dias <24493328+acidiney@users.noreply.github.com>
2025-12-04 12:31:26 +0000

    fix: send email

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -6,10 +6,13 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   await resend.emails.send({
-    from: `${body.name} <${body.email}>`,
+    from: `Contact Form <no-reply@acidineydias.dev>`,
     to: ["hireme@acidineydias.dev"],
     subject: "Contact Form Submission",
-    html: `<p>${body.message}</p>`,
+    html: `
+           <p>From: ${body.name} (${body.email})</p>
+           <p>${body.message}</p>
+        `,
   });
 
   return { success: true };
