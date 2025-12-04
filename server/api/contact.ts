c2dfb43865c708b05b665516a351c0659812c6bd
Acidiney Dias <24493328+acidiney@users.noreply.github.com>
2025-12-04 12:33:55 +0000

    chore: move mails to env variables

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -1,13 +1,15 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
+const noReplyEmail = process.env.NO_REPLY_EMAIL!;
+const contactRecipientEmail = process.env.CONTACT_RECIPIENT_EMAIL!;
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
   await resend.emails.send({
-    from: `Contact Form <no-reply@acidineydias.dev>`,
-    to: ["hireme@acidineydias.dev"],
+    from: `Contact Form <${noReplyEmail}>`,
+    to: [contactRecipientEmail],
     subject: "Contact Form Submission",
     html: `
            <p>From: ${body.name} (${body.email})</p>
